69dc3362312f3071c764cd69196f17fd96c31a73
Ilya <mr.flay0077@yandex.ru>
2023-01-27 18:38:52 +0300

    Seminar4Zadacha3

---
+++ Seminar4Zadacha3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.*/
+int [] mas = new int[8];
+Console.Write("[");
+for (int i = 0; i < mas.Length; i++)
+ {
+    mas [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+int Method (int a)
+{
+    return mas[a];
+}
