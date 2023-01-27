27f5ff1a0a5783e9a382b4d1905babd719ea3cc1
Ilya <mr.flay0077@yandex.ru>
2023-01-27 16:43:55 +0300

    Seminar4Zadacha2

---
+++ Seminar4Zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+int Prompt(string msg)
+{
+    Console.Write($"{msg}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = Prompt("Введите число");
+int sum = 0;
+while (A > 0)
+{
+    int num = A % 10;
+    A = A / 10;
+    sum = sum + num;
+}
+Console.WriteLine("Сумма всех цифр в числе равна: " + sum);
