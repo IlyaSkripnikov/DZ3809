8dd03bfc4c6c2e440d6ff5bedddfeb1c037a7138
Ilya <mr.flay0077@yandex.ru>
2023-02-02 17:05:10 +0300

    Seminar5Zadacha3

---
+++ Seminar5Zadacha3/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.//
+double[] GenerateArray(int len = 5)
+{
+    double[] array = new double[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.NextDouble()*10;
+    }
+    return array;
+}
+double Difference (double[] array)
+{
+    double min = Int32.MaxValue;
+    double max = Int32.MinValue;
+    double count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        
+    }
+    count = max - min;
+    return count;
+}
+void PrintArray(double[] array)
+{
+     foreach (double numbers in array)
+    {
+        Console.Write($"{numbers}\t");
+    }
+    Console.WriteLine();
+}
+double[] array = GenerateArray();
+PrintArray(array);
+System.Console.WriteLine($"Разница элементов: {Difference(array)}");
+
