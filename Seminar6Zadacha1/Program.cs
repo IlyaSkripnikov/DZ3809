7e9adf18f89495fcb291bbb7093040a1b0f39de0
Ilya <mr.flay0077@yandex.ru>
2023-02-04 15:11:43 +0300

    Seminar6Zadacha1

---
+++ Seminar6Zadacha1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int InputInt(string msg)
+{
+    System.Console.Write($"{msg}");
+    return int.Parse(Console.ReadLine());
+}
+int[] GenerateArray(int len)
+{
+  int[] array = new int[len];
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"Введите число {i + 1}: ");
+    array[i] = int.Parse(Console.ReadLine());
+  }
+  return array;
+}
+int PositiveNum(int[] number)
+{
+  int Positive = 0;
+  for (int i = 0; i < number.Length; i++)
+  {
+    if (number[i] > 0)
+    {
+      Positive++;
+    }
+  }
+  return Positive;
+}
+void PrintArray(int[] array)
+{
+  foreach (int item in array)
+  {
+    System.Console.Write($"{item}\t");
+  }
+  System.Console.WriteLine();
+}
+int num = InputInt("Какое кол-во чисел ввести: ");
+int[] array = GenerateArray(num);
+PrintArray(array);
+int positiveCount = PositiveNum(array);
+Console.WriteLine($"Кол-во чисел больше 0: {positiveCount}");
