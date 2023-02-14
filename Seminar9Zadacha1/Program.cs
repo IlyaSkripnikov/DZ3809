5dc104804132dfc9e5d5092a61d8e84f945d7cbe
Ilya <mr.flay0077@yandex.ru>
2023-02-14 21:44:32 +0300

    Seminar9Zadacha1

---
+++ Seminar9Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте значения M и N. Напишите программу,
+//которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+void ShowNumbers(int start, int finish)
+{
+    if (start > finish)
+    {
+        return;
+    }
+    System.Console.Write(start + " ");
+    ShowNumbers(start + 2, finish);
+}
+ShowNumbers(0, 10);
